ea2a7a61e7c3d20ee97aad6492b124484ac7062d
Saloni-singh-08 <168645304+Saloni-singh-08@users.noreply.github.com>
2025-05-13 15:36:38 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";    //treat all JS code as newer version
+
+// alert( 3 + 3) // we are using node not browser
+
+console.log(3 + 3)
+
+console.log(typeof null);
+console.log(typeof "hitesh");
+console.log(typeof "string");
+console.log(typeof 10);
